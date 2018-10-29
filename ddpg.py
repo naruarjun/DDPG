261c5135dcf1e4fd6dc30015c992806fffe0cba3
abhay <abhayrawat2695@gmail.com>
2018-10-29 12:26:57 +0530

    flake8

--- ddpg.py
+++ ddpg.py
@@ -1,20 +1,74 @@
-import tensorflow as tf
-import tf.float32 as f32
 import yaml
-from baselines.ddpg.models import Actor, Critic
+from collections import OrderedDict
+
+import tensorflow as tf
+from tensorflow.contrib.staging import StagingArea
+from tensorflow import float32 as f32
+
+from models import Actor, Critic
+
 
 class DDPG:
-  def __init__(self, config):
-    self.params = yaml.loads(config)
-    self.actor  = Actor(**params["actor"])
-    self.critic = Critic(**params["critic"])
-    self.buffer = ReplayBuffer(**params["buffer"])
-    self.create_tf_vars()
 
-  def create_tf_vars(self)
-    self.obs0   = tf.placeholder(f32, shape=(None, self.params["d_obs"]), name="obs0")
-    self.obs1   = tf.placeholder(f32, shape=(None, self.params["d_obs"]), name="obs1")
-    self.reward = tf.placeholder(f32, shape=(None, self.params["d_ret"]), name="rets")
-    self.action = tf.placeholder(f32, shape=(None, self.params["d_act"]), name="actn")
-    self.goal   = tf.placeholder(f32, shape=(None, self.params["d_gol"]), name="goal")
-    self.a_goal = tf.placeholder(f32, shape=(None, self.params["d_gol"]), name="a_gl")
+    def __init__(self, params):
+        self.params = params
+        # Create shape dict for inputs
+        self.create_shape_dict()
+        # create Staging area
+        self.create_stageing_area()
+        # create actor/critic models
+        self.actor = Actor(self.inputs, **params["actor"], name="ACTOR")
+        self.critic = Critic(self.inputs, self.actor,
+                             **params["critic"], name="CRITIC")
+        # create their corresponding target models
+        self.t_actor = Actor(self.inputs, **params["actor"], name="T_ACTOR")
+        self.t_critic = Critic(self.inputs, self.t_actor,
+                               **params["critic"], name="T_CRITIC")
+        # Training rules
+        # for Critic
+        # for Actor
+        # self.setup_trainers()
+        # create target update rules
+
+    def create_shape_dict(self):
+        self.shape_dict = OrderedDict()
+        for key, shape in sorted(params["dimensions"].items()):
+            self.shape_dict[key] = (None, shape)
+        print("SHAPE DICT\n", self.shape_dict)
+
+    def create_stageing_area(self):
+        self.staging_area = StagingArea(dtypes=[f32 for _ in self.shape_dict],
+                                        shapes=self.shape_dict.values())
+        self.staging_area.put([tf.placeholder(f32, shape=shape, name=name)
+                              for name, shape in self.shape_dict.items()])
+        self.inputs = self.staging_area.get()
+        self.inputs = {k: self.inputs[i] for i, k
+                       in enumerate(self.shape_dict.keys())}
+
+
+def load_config(filename):
+    with open(filename) as f:
+        config = yaml.load(f.read())
+    return config
+
+
+params = load_config("config.yaml")
+print(params)
+ddpg = DDPG(params)
+a, c = ddpg.actor, ddpg.critic
+# ovar = create_input_dict(params)
+# sa = create_stageing_area(ovar)
+
+# b = sa.get()
+# inputs = {k: b[i] for i, k in enumerate(ovar.keys())}
+
+
+# s = DDPG(inputs, params)
+# a = s.actor
+# c = s.critic
+
+
+writer = tf.summary.FileWriter("__tensorboard/", tf.get_default_graph())
+# print (op)
+writer.close()
+print(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES))
