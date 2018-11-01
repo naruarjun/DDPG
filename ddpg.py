eaf26c51e6e71d512e4bdaa69bfa74b974f2bc98
abhay <abhayrawat2695@gmail.com>
2018-11-01 16:51:07 +0530

    removed uncessary prints

--- ddpg.py
+++ ddpg.py
@@ -41,11 +41,9 @@ class DDPG:
     def step(self, x, is_u_discrete, explore=True):
         x = x.reshape(-1, self.dimensions["x"])
         u = self.actor.predict(x)
-        # print("Action before noise:{} ou_level: {}".format(u, self.ou_level))
         if explore:
             self.ou_level = self.noise.ornstein_uhlenbeck_level(self.ou_level)
             u = u + self.ou_level
-        # print("Action after noise: {} ou_level: {}".format(u, self.ou_level))
         q = self.critic.predict(x, u)
         if is_u_discrete:
             return [np.argmax(u), u[0], q[0]]
@@ -57,27 +55,12 @@ class DDPG:
     def train(self):
         # check if the memory contains enough experiences
         if self.memory.size < self.b_size:
-            print("Memory size insufficient")
             return
         x, u, r, nx, t = self.get_batch()
-        # print(x)
-        # print(u)
-        # print(r)
-        # print(nx)
-        # print(t)
-        # print(1-t)
-        # t += 1
         nu = self.actor.predict_target(nx)
-        # print(nu)
         tq = r + self.gamma*self.critic.predict_target(nx, nu)*(1-t)
-        # print(np.amax(tq))
-        # import sys
-        # sys.exit(0)
-        # print("PRED Q", self.critic.predict_target(nx, nu))
-        # print("TARGET Q ", tq)
         self.critic.train(x, u, tq)
         g = self.critic.get_action_grads(x, u)
-        # print("G: ", g)
         self.actor.train(x, g)
         self.update_targets()
 
@@ -88,5 +71,3 @@ class DDPG:
     def update_targets(self):
         self.critic.update_target()
         self.actor.update_target()
-
-# dd = DDPG(tf.Session(), load_config("config.yaml"))
