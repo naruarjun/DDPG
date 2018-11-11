097cd645713f9fb46e09cbf9a2c5ebbf01359769
aarg <abhayrawat2695@gmail.com>
2018-11-11 17:38:18 +0530

    ..

--- go2goal.py
+++ go2goal.py
@@ -1,6 +1,7 @@
 import gym
 from gym.spaces import Box
 from gym.envs.registration import EnvSpec
+from gym.envs.registration import register
 
 import numpy as np
 from numpy.linalg import norm
@@ -16,7 +17,9 @@ from ray.tune.registry import register_env
 
 class Go2Goal(gym.Env):
 
-    def __init__(self, config):
+    cossin = staticmethod(lambda x: np.array([np.cos(x), np.sin(x)]))
+
+    def __init__(self, config=None):
         """
         Simple go to goal environment where a non-holonomic agent is
         required to move to a goal. The goal can be random or single,
@@ -34,33 +37,38 @@ class Go2Goal(gym.Env):
         """
         # Default values. Will be overridden if specified in config
         self.dt = 1e-2
-        self.her = False
-        self.seed = None
-        self.d_clip = 1
-        self.thresh = 0.1
+        # self.her = True
+        self.her = True
+        # self.seed = None
+        self.thresh = np.array([0.05, 0.05, 0.1])[:-1]
         self.num_iter = 50
-        self.reward_max = 10
-        self.step_penalty = 0.0
-        self.max_episode_steps = 120
-        self._max_episode_steps = 120
+        self.reward_max = 1
+        self.max_episode_steps = 50
+        self._max_episode_steps = 50
+        self.step_penalty = 1./(self.max_episode_steps*2)
 
-        self.action_low = np.array([-0.1, -np.pi/4])
+        self.action_low = np.array([0.0, -np.pi/4])
         self.action_high = np.array([0.3, np.pi/4])
         self.action_space = Box(self.action_low, self.action_high, dtype="f")
 
+        # so that the goals are within the range of performing actions
+        self.d_clip = self.action_high[0]*self.num_iter*self.dt*1.35
+
         self.observation_space = Box(low=-1, high=1, shape=(5,), dtype="f")
 
-        self.limits = np.array([2.5, 2.5, 2*np.pi])
+        self.limits = np.array([1, 1, np.pi])
         self.agent = Agent(0)
-        self.__dict__.update(config)
+        if config is not None:
+            self.__dict__.update(config)
 
-        if self.seed is not None:
-            np.random.seed(self.seed)
+        # if self.seed is not None:
+        #     np.random.seed(self.seed)
 
         self.goal = None
         if not self.her:
             self.dMax = self.action_high[0]*self.dt*self.num_iter
             self.dRange = 2*self.dMax
+        self.viewer = None
         self._spec = EnvSpec("Go2Goal-v0")
 
     def reset(self):
@@ -69,34 +77,78 @@ class Go2Goal(gym.Env):
 
         # print("Agent spawned at: ", self.agent.pose)
         # print("Goalpoint set to: ", self.goal)
-        return self.compute_obs(self.agent.pose)
+        return self.compute_obs()
 
     def step(self, action):
         assert self.goal is not None, "Call reset before calling step"
         prev_pose = Pose(*self.agent.pose.tolist())
-        prev_dist = self.get_current_distance()
-        self.agent.step(action)
-        reward, done = self.get_reward(prev_dist)
-        # print("reward: ", reward)
+        for i in range(self.num_iter):
+            self.agent.step(action)
+        reward, done = self.get_reward()
         return self.compute_obs(prev_pose), reward, done,\
-            {"dist": self.current_distance}
+            {"dist": self.current_distance, "is_success": done}
 
-    def get_reward(self, prev_distance):
-        did = prev_distance - self.get_current_distance()
-        reached = self.get_current_distance() < self.thresh
-        reward = self.reward_max if reached else 0.0
-        if not self.her:
-            reward = ((did + self.dMax)/self.dRange*2 - 1)*self.reward_max
-        # print("p: {}\tc: {}\td: {}".format(prev_distance, self.get_current_distance(), did))
-        # print(reward)
-        return reward-self.step_penalty, reached
+    def get_reward(self):
+        reached = (self.distance_from_goal() < self.thresh).all()
+        if reached:
+            return self.reward_max, True
+        return -self.step_penalty, False
+        # return -self.step_penalty if not reached else self.reward_max
+        # reward = self.reward_max if reached else 0.0
+        # return reward-self.step_penalty, reached
 
-    def get_current_distance(self):
-        self.current_distance = norm((self.agent.pose - self.goal)[:-1])
+    def distance_from_goal(self):
+        self.current_distance = np.abs((self.agent.pose - self.goal))[:-1]
         return self.current_distance
 
     def sample_pose(self):
         return Pose(*(random(3)*self.limits - self.limits/2))
+
+    def render(self, mode='human'):
+        screen_width = 500
+        screen_height = 500
+
+        world_width = 2.5
+        scale = screen_width/world_width
+        if self.goal is None: return None
+        c, theta = np.split(self.goal.tolist(), [2])
+        a, atheta = np.split(self.agent.pose.tolist(), [2])
+        sh = 20
+        ss = 5
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+            vs = [c+Go2Goal.cossin(0)*sh, c+Go2Goal.cossin(-np.pi/2)*ss, c+Go2Goal.cossin(np.pi/2)*ss]
+
+            goal = rendering.FilledPolygon([tuple(i) for i in vs])
+            goal.set_color(.3,.82,.215)
+            self.visual_goal_trans = rendering.Transform()
+            goal.add_attr(self.visual_goal_trans)
+            self.viewer.add_geom(goal)
+
+            agent = rendering.FilledPolygon([tuple(i) for i in vs])
+            agent.set_color(.15,.235,.459)
+            self.visual_agent_trans = rendering.Transform()
+            agent.add_attr(self.visual_agent_trans)
+            self.viewer.add_geom(agent)
+
+            for i in np.arange(-1.5, 1.5, step=0.5):
+                pt = i*scale + screen_width/2.
+                line = rendering.Line((0, pt), (screen_width, pt))
+                line.set_color(0, 0, 0)
+                self.viewer.add_geom(line)
+                pt = i*scale + screen_width/2.
+                line = rendering.Line((pt, 0), (pt, screen_width))
+                line.set_color(0, 0, 0)
+                self.viewer.add_geom(line)
+
+        self.visual_goal_trans.set_translation(*(c*scale+screen_width/2.0))
+        self.visual_goal_trans.set_rotation(theta)
+        self.visual_agent_trans.set_translation(*(a*scale+screen_width/2.0))
+        self.visual_agent_trans.set_rotation(atheta)
+
+        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+
 
     def compute_obs(self, prev_pose=None):
         # our observations are going to be the distance to be moved in
@@ -106,8 +158,8 @@ class Go2Goal(gym.Env):
         goal_vec, angle = np.split((self.goal - self.agent.pose), [-1])
         distance = np.linalg.norm(goal_vec)
         unit_vec = goal_vec / distance if distance != 0 else goal_vec
-        c_dist = min(distance, self.d_clip)/self.d_clip
-        sc = np.hstack([np.cos(angle), np.sin(angle)])
+        c_dist = min(distance, self.d_clip)#/self.d_clip
+        sc = np.hstack([np.cos(self.agent.pose.theta), np.sin(self.agent.pose.theta)])
         goal = np.hstack([unit_vec, c_dist])
         if not self.her:
             return np.hstack([sc, goal])
@@ -115,11 +167,40 @@ class Go2Goal(gym.Env):
             ag = np.zeros(3)
         else:
             ag = (self.agent.pose - prev_pose)[:-1]
-            ag = np.hstack([ag/norm(ag), norm(ag)])
-        return {"obs": sc,
-                "goal": goal,
-                "ag": ag}
+            if norm(ag) < 1e-6:
+                print("++"*50)
+                print(self.agent.pose)
+                print(prev_pose)
+                print(self.goal)
+                print(ag)
+                ag += 1e-5
+                print("++"*50)
 
+            ag = np.hstack([ag/norm(ag), norm(ag)])
+        return {"observation": sc,
+                "desired_goal": goal,
+                "achieved_goal": ag}
+
+    def close(self):
+            if self.viewer:
+                self.viewer.close()
+                self.viewer = None
+
+    def seed(self, seed):
+        np.random.seed(seed)
+
+    def compute_reward(self, achieved_goal, desired_goal, info):
+        # print(achieved_goal.shape)
+        # print(desired_goal.shape)
+        r = norm(achieved_goal - desired_goal, axis=1)
+        # print(r, r.shape, r.astype('i'))
+        return (r < self.thresh).astype('i')
+
+
+register(
+    id='Go2Goal-v0',
+    entry_point='go2goal:Go2Goal',
+)
 
 if __name__ == "__main__":
     register_env("Go2Goal-v0", lambda config: Go2Goal(config))
@@ -136,8 +217,8 @@ if __name__ == "__main__":
                 "schedule_max_timesteps": 1000000,
                 "timesteps_per_iteration": 120,
                 "exploration_fraction": 0.2,
-                "gpu": True,
-                "num_workers": 1,
+                "gpu": False,
+                "num_workers": 11,
                 # "env_config": {
                 #     "seed": 5,
                 # },
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# import gym
+import gym
 import yaml
 import numpy as np
 import tensorflow as tf
@@ -6,8 +6,8 @@ from tensorflow.summary import FileWriter
 
 from ddpg import DDPG
 # from reward import Reward
-from environment import G2Goal
-
+# from environment import G2Goal
+import go2goal
 
 def load_config(filename):
     with open(filename) as f:
@@ -20,24 +20,18 @@ def scale_action(u):
 
 
 def train(config):
-    # reward_normalizer = Reward(0.1, config["gamma"])
-    # env = gym.make(config["env_name"])
-    env = G2Goal()
-    # env.seed(134)
-    # visual = Visualizer(env.env, 4, 1, [-5, 5, -5, 5])
-    # visual.thread.start()
+    env = gym.make("Go2Goal-v0")
     is_u_discrete = len(env.action_space.shape) == 0
     tf_session = tf.Session()
     ddpg_agent = DDPG(tf_session, config)
     tf_session.run(tf.global_variables_initializer())
 
-    # print(ddpg_agent.actor.pi.net_params)
-    # exit()
+    print(config.keys())
     saver = tf.train.Saver()
     summarizer = FileWriter("__tensorboard/her2", tf_session.graph)
     s_summary = tf.Summary()
-    log_str = "| [{}] Episode: {:4} | Reward: {:7d} | Q: {:8.3f} | T: {:3d} | MIND: {:4.3f} |"
-    # log_str_e = "| [{}] EvalRun: {:4} | Reward: {:4.2f} | Q: {:8.3f} |"
+    log_str = "| [{}] Episode: {:4} | Reward: {:7.3f} | Q: {:8.3f} | T: {:3d} | MIND: {:4.3f} |"
+
     summary_op = tf.summary.merge_all()
 
     # for testing purposes!!!
@@ -47,29 +41,20 @@ def train(config):
         episodic_q = 0.
         obs = env.reset()
         episode_batch = []
-        min_d2goal = env.get_current_distance()
+        min_d2goal = env.distance_from_goal()
         for i in range(env._max_episode_steps):
             # print(obs)
-            action, u, q = ddpg_agent.step(np.hstack([obs["obs"],
-                                           obs["goal"]]),
+            action, u, q = ddpg_agent.step(np.hstack([obs["observation"],
+                                           obs["desired_goal"]]),
                                            is_u_discrete)
             episodic_q += q
-            # print("ACTION BEFORE: ", action)
             action = scale_action(action)
-            # print("ACTION AFTER : ", action)
-            # print(env.env.agents[0].pose)
             new_obs, r, done, info = env.step(action)
-            # print(action)
-            # print(env.env.agents[0].pose)
-            # print(new_obs, r, done)
-            ogag = [obs[k] for k in ["obs", "goal", "ag"]]
-            episode_batch.append([*ogag, u, r, new_obs["obs"],
-                                  new_obs["goal"], int(done)])
-            if info["dist"] < min_d2goal:
+            ogag = [obs[k] for k in ["observation", "desired_goal", "achieved_goal"]]
+            episode_batch.append([*ogag, u, r, new_obs["observation"],
+                                  new_obs["desired_goal"], int(done)])
+            if (info["dist"] < min_d2goal).all():
                 min_d2goal = info["dist"]
-            # print([*ogag, u, r, new_obs["obs"], int(done)])
-            # if episode == 2:
-            #     exit()
             obs = new_obs
             if "render" in config.keys() and config["render"]:
                 env.render()
@@ -86,8 +71,8 @@ def train(config):
         # n_batch = reward_normalizer.discount(episode_batch)
         for experience in episode_batch:
             ddpg_agent.remember(experience)
-        print(log_str.format("T", episode+1, int(episodic_r),
-                             float(episodic_q), i+1, min_d2goal))
+        print(log_str.format("T", episode+1, episodic_r,
+                             float(episodic_q), i+1, np.linalg.norm(min_d2goal)))
         summarizer.add_summary(tf_session.run(summary_op), episode)
         summarizer.flush()
         # To run or not to run evaluations on current target policy...
@@ -101,7 +86,7 @@ def train(config):
             eval_q = 0.
             obs = env.reset()
             for j in range(env._max_episode_steps):
-                u, _, q = ddpg_agent.step(np.hstack([obs["obs"], obs["goal"]]),
+                u, _, q = ddpg_agent.step(np.hstack([obs["observation"], obs["desired_goal"]]),
                                           is_u_discrete, explore=False)
                 obs, r, done, _ = env.step(u)
                 eval_score += r
@@ -110,7 +95,7 @@ def train(config):
                     break
             m_eval_q += eval_q
             m_eval_score += eval_score
-            print(log_str.format("E", eval_run+1, int(eval_score),
+            print(log_str.format("E", eval_run+1, m_eval_score,
                                  float(m_eval_q), j+1, -1))
         print()
         # save the model checkpoints if they are the current best...
