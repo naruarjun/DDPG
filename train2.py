9284fe025c53826c56b396782a08d9f54c597b84
aarg <abhayrawat2695@gmail.com>
2018-11-15 16:41:30 +0530

    Resume training from chkpt

--- train2.py
+++ train2.py
@@ -1,5 +1,7 @@
 import gym
 import yaml
+import argparse
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.summary import FileWriter
@@ -7,6 +9,21 @@ from tensorflow.summary import FileWriter
 from ddpg import DDPG
 import go2goal
 from rollout import RolloutGenerator
+
+
+EXAMPLE_USAGE = """
+Train agent using DDPG + HER
+Example Usage:
+* Train agent using a specific configuration
+(See config for more details)
+
+python train.py --config some_config.yaml
+
+* Resume training from a specific checkpoint
+(Only specify the name. No extensions needed)
+
+python train.py --config some_config.yaml --from-ckpt __checkpoints/some-ckpt
+"""
 
 
 def load_config(filename):
@@ -24,8 +41,26 @@ def scale_action_gen(env, u_min, u_max):
     return scale_action
 
 
-def train(config):
-    from_ckpt = "__checkpoints/P_2900"
+def create_parser(parser_creator=None):
+    parser = parser = argparse.ArgumentParser(
+             formatter_class=argparse.RawDescriptionHelpFormatter,
+             description="Train a reinforcement learning agent.",
+             epilog=EXAMPLE_USAGE)
+    parser.add_argument(
+        "--config",
+        default=None,
+        type=str,
+        help="Config file.")
+    parser.add_argument(
+        "--from-ckpt",
+        default=None,
+        type=str,
+        help="Checkpoint file if training is to"+\
+             " be resumed from checkpoint")
+    return parser
+
+
+def train(config, from_ckpt=None):
     tf_session = tf.Session()
     env = gym.make("Go2Goal-v0")
     scale_action = scale_action_gen(env, np.ones(2)*-1, np.ones(2))
@@ -53,9 +88,10 @@ def exit():
 
 
 def main():
-    config = load_config("config_g2g.yaml")
-    print()
-    print(train(config))
+    parser = create_parser()
+    args = parser.parse_args()
+    config = load_config(args.config)
+    print(train(config, args.from_ckpt))
 
 
 if __name__ == "__main__":
