763f2335b612263ee8e8d41122920844697767a7
aarg <abhayrawat2695@gmail.com>
2018-12-05 12:42:24 +0530

    modified for this problem

--- actor.py
+++ actor.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 
-from tensorflow import multiply as mul
 
+from tensorflow import float32 as f
+from tensorflow import multiply as mul
 from tensorflow.train import AdamOptimizer as Adam
 from tensorflow.initializers import truncated_normal as TN
 
@@ -11,27 +12,29 @@ from FCNN import FCNN
 class Actor:
     def __init__(self, sess, input_t, **params):
         self.session = sess
-        self.load_from_ckpt = False
+        self.inputs = input_t
         self.__dict__.update(params)
-        self.__dict__.update(input_t)
         self.generate_networks()
         self.define_operations()
 
     def generate_networks(self, load_from_ckpt=False):
+        # Concatenate state and goals for policy input
+        a_input = [self.inputs[k] for k in sorted(self.inputs.keys())]
+        a_input = tf.concat(a_input, axis=1)
         # MAIN ACTOR NETWORK
-        self.pi = FCNN(self.x, self.u.shape[-1], self.n_layers, self.n_units,
-                       tf.nn.relu, tf.nn.tanh, name="pi",
-                       w_init=TN(stddev=1e-1), from_ckpt=self.load_from_ckpt)
+        self.pi = FCNN(a_input, self.dim_action, self.n_layers,
+                       self.n_units, tf.nn.relu, tf.nn.tanh, name="pi")
         # TARGET ACTOR NETWORK
-        self.PI = FCNN(self.x, self.u.shape[-1], self.n_layers, self.n_units,
-                       tf.nn.relu, tf.nn.tanh, name="t_pi",
-                       from_ckpt=self.load_from_ckpt)
+        self.PI = FCNN(a_input, self.dim_action, self.n_layers,
+                       self.n_units, tf.nn.relu, tf.nn.tanh, name="t_pi")
+        self.grads = tf.placeholder(f, shape=(None, self.dim_action),
+                                    name="grad") 
 
     def define_operations(self):
         with tf.name_scope("actor_ops"):
             # GRADIENT OF ACTIONS WRT ACTOR PARAMS TIMES NEGATIVE GRADIENT OF
             # VALUE FUNCTION WRT ACTIONS
-            grads = tf.gradients(self.pi.nn, self.pi.net_params, -self.g)
+            grads = tf.gradients(self.pi.nn, self.pi.net_params, -self.grads)
             # APPLY GRADIENTS TO ACTOR NETWORK
             self.optimize = Adam(self.lr, name="pi_adam")\
                 .apply_gradients(zip(grads, self.pi.net_params))
@@ -41,17 +44,18 @@ class Actor:
                 self.updt_PI = [j.assign(mul(self.tau, i)+mul((1-self.tau), j))
                                 for i, j in net_param_pairs]
 
-    def predict(self, x):
-        return self.session.run(self.pi.nn,
-                                feed_dict={self.x: x})
+    def predict(self, inputs):
+        values = {v: inputs[k] for k, v in self.inputs.items()}
+        return self.session.run(self.pi.nn, feed_dict=values)
 
-    def predict_target(self, x):
-        return self.session.run(self.PI.nn,
-                                feed_dict={self.x: x})
+    def predict_target(self, inputs):
+        values = {v: inputs[k] for k, v in self.inputs.items()}
+        return self.session.run(self.PI.nn, feed_dict=values)
 
-    def train(self, x, g):
-        return self.session.run(self.optimize,
-                                feed_dict={self.x: x, self.g: g})
+    def train(self, inputs, grads):
+        values = {v: inputs[k] for k, v in self.inputs.items()}
+        values[self.grads] = grads
+        return self.session.run(self.optimize, feed_dict=values)
 
     def update_target(self):
         self.session.run(self.updt_PI)
--- critic.py
+++ critic.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+from tensorflow import float32 as f
 from tensorflow import square as sq
 from tensorflow import multiply as mul
 from tensorflow import reduce_mean as rmean
@@ -7,44 +8,47 @@ from tensorflow import reduce_mean as rmean
 from tensorflow.train import AdamOptimizer as Adam
 
 from FCNN import FCNN
+from util import info
 
 
 class Critic:
     def __init__(self, sess, input_t, **params):
         self.session = sess
-        self.input_t = input_t
+        self.inputs = input_t
         self.__dict__.update(params)
-        self.__dict__.update(input_t)
         self.generate_networks()
         self.define_operations()
 
     def generate_networks(self):
-        q_input = tf.concat([self.x, self.u], axis=1)
+        q_input = [self.inputs[k] for k in sorted(self.inputs.keys())]
+        q_input = tf.concat(q_input, axis=1)
         # MAIN CRITIC NETWORK
         self.q = FCNN(q_input, 1, self.n_layers, self.n_units,
                       tf.nn.relu, name="q")
         # TARGET CRITIC NETWORK
         self.Q = FCNN(q_input, 1, self.n_layers, self.n_units,
                       tf.nn.relu, name="t_q")
+        self.pred_q = tf.placeholder(f, shape=(None, 1), name="pred_q")
 
     def define_operations(self):
         with tf.name_scope("critic_ops"):
             # LOSS
-            loss = tf.sqrt(rmean(sq(self.p - self.q.nn)))
+            loss = tf.sqrt(rmean(sq(self.pred_q - self.q.nn)))
             # MINIMIZE LOSS OP
             self.minimize = Adam(self.lr, name="q_adam")\
                 .minimize(loss, var_list=self.q.net_params)
             # ACTION GRADIENTS
-            self.action_grads = tf.gradients(self.q.nn, self.u, name="dq_du")
+            a_list = [x for x, y in self.inputs.items() if "action" in x]
+            self.action_grads = tf.gradients(self.q.nn, a_list, name="dq_du")
             # UPDATE TARGET OP
             net_param_pairs = zip(self.q.net_params, self.Q.net_params)
             with tf.name_scope("update_target_q"):
                 self.updt_Q = [j.assign(mul(self.tau, i)+mul((1-self.tau), j))
                                for i, j in net_param_pairs]
 
-    def predict(self, x, u):
-        return self.session.run(self.q.nn,
-                                feed_dict={self.x: x, self.u: u})
+    def predict(self, inputs):
+        values = {v: inputs[k] for k, v in self.inputs.items()}
+        return self.session.run(self.q.nn, feed_dict=vals)
 
     def predict_target(self, x, u):
         return self.session.run(self.Q.nn,
